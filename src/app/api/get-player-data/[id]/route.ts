2e796ccf523c4fafa4e2fd2c3a5d2d78120a6f04
Matthew <glowupmatt@gmail.com>
2024-01-12 19:30:29 -0800

    add auth check to get-player-data

--- src/app/api/get-player-data/[id]/route.ts
+++ src/app/api/get-player-data/[id]/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
 import prisma from "@/utils/prismaDb";
+import { noAuth } from "@/utils/hasAuth";
 
 export const GET = async (
   request: Request,
   { params }: { params: { id: string } }
 ) => {
+  if (noAuth(request)) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
   try {
     const { id } = params;
     const player = await prisma.player.findUnique({
